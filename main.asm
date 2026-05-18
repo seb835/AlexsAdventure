ae76f12e387608512a9c613ad4e8a7ac57f8ed1b
Seb Pedley <seb@propeller-studios.co.uk>
2026-05-18 10:21:21 +0100

    Basic folder structure with placeholder files

--- main.asm
+++ main.asm
@@ -2,6 +2,9 @@
 // --------------- Alex's Adventure ---------------
 
 #import "/system/memoryMap.asm"
+#import "/system/config.asm"
+#import "/includes/constants.asm"
+#import "/includes/macros.asm"
 
 BasicUpstart2(main)
 
@@ -10,3 +13,21 @@ BasicUpstart2(main)
 
 *=VARIABLES_ADDRESS "Variables"
 #import "/includes/variables.asm"
+
+*=CHARSET_ATTRIB_ADDRESS "Charset Attrib"
+.import binary "/charset/charsetAttrib.bin"
+
+*=TILESET_ADDRESS "Tileset"
+.import binary "/tileset/tileset.bin"
+
+*=SPRITES_ADDRESS "Sprites"
+.import binary "/sprites/sprites.bin"
+
+*=CHARSET_ADDRESS "Charset"
+.import binary "/charset/charset.bin"
+
+*=TABLES_ADDRESS "Tables"
+#import "/includes/tables.asm"
+
+*=LIBRARIES_ADDRESS "Libraries"
+#import "/includes/libraries.asm"
